49b1fe48b9a9c700232b2a14f690a271e8f1efef
nihadaslanli <nihadsasl@code.edu.az>
2025-01-13 18:53:08 +0400

    Add project files.

---
+++ Algorithm tasks 11-19/Program.cs
@@ -0,0 +1,24 @@
+﻿//#region task
+//int num=int.Parse(Console.ReadLine());
+//for (int i = 0; i < num; i++)
+//{if()
+
+//}
+//#endregion
+#region task9
+//const int toplamAyak = 64;
+//const int qazAyak = 2;   
+//const int dovsanAyak = 4;
+
+//Console.WriteLine("Qazların ve dovşanların mümkün kombinasyonları:");
+
+//for (int qaz = 0; qaz <= toplamAyak / qazAyak; qaz++)
+//{
+//    int dovsan = (toplamAyak - (qaz * qazAyak)) / dovsanAyak;
+
+//    if (qaz * qazAyak + dovsan * dovsanAyak == toplamAyak)
+//    {
+//        Console.WriteLine($"Qaz: {qaz}, Dovşan: {dovsan}");
+//    }
+//}
+#endregion
